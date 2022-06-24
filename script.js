7fd66aaaa7257eac85868e169741cca8f08078f9
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-06-24 16:41:56 -0700

    Add length prompt

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specCharList = ["!", "\"", "#", "$", "&", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+
+var userChoiceObject = {
+  lengthChoice: 0,
+  lowercaseChoice: true,
+  uppercaseChoice: true,
+  numericChoice: true,
+  specCharChoice: true,
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,6 +18,36 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+//Create password from prompt choices
+function generatePassword() {
+  generateLengthPrompt();
+  generateCharPrompt();
+
+}
+
+//Create prompt for length of password
+function generateLengthPrompt() {
+  var userLengthChoice = window.prompt("Enter password length (between 8 and 128 characters):");
+  var stillNeedLength = true;
+  do {
+    if (!isNaN(userLengthChoice) && userLengthChoice < 8) {
+      userLengthChoice = window.prompt("Please enter a password length between 8 and 128 characters:");
+    } else if (!isNaN(userLengthChoice) && userLengthChoice > 128) {
+      userLengthChoice = window.prompt("Please enter a password length between 8 and 128 characters:");
+    } else if (!isNaN(userLengthChoice) && userLengthChoice > 8 && userLengthChoice < 128) {
+      stillNeedLength = false;
+      return userLengthChoice;
+    } else if (isNaN(userLengthChoice)){
+      window.alert("Must enter a number.");
+      userLengthChoice = window.prompt("Please enter a password length between 8 and 128 characters:");
+    }
+  } while (stillNeedLength)
+}
+
+function checkLengthChoice(userLength) {
 
 }
 
